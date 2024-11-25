cdb1ff13bb0a352ad685698f05cd815f4b716b5d
SergeyOsin2005 <sereosi2017@yandex.ru>
2024-11-25 18:14:16 +0300

    Создан класс Квадрат и протестировано в главном файле

---
+++ Cheburyshka/Square.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Figure.h"
+class Square : public Figure {
+public:
+	Square(Point t, int a, COLORREF b, COLORREF);
+	void draw() override;
+private:
+	int x3, y3;
+	int x4, y4;
+	COLORREF bc, wc;
+};
