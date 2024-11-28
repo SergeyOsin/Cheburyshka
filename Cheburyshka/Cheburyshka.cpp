68e46890731ea36b227d223a1fb38249a85eeb22
Sergey <rihlerstudy@gmail.com>
2024-11-28 18:25:09 +0300

    Добавлен класс Чебурашка

--- Cheburyshka/Cheburyshka.cpp
+++ Cheburyshka/Cheburyshka.cpp
@@ -1,14 +1,13 @@
-﻿#include <iostream>
-#include "Figure.h"
-#include "Square.h"
-#include "Triangle.h"
-#include "Octagon.h"
-#include "Circle.h"
-using namespace std;
-int main()
+#include "Cheburyshka.h"
+
+Cheburyshka::Cheburyshka(Point _t, int a, COLORREF c1, COLORREF c2)
 {
-    system("pause");
-    return 0;
+	body = Body({ _t.x, _t.y + a }, a, c1);
+	head = Head(_t, a, c2, c1);
 }
 
-
+void Cheburyshka::draw()
+{
+	head.draw();
+	body.draw_body();
+}
---
+++ Cheburyshka/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Figure.h"
+#include "Square.h"
+#include "Triangle.h"
+#include "Octagon.h"
+#include "Circle.h"
+using namespace std;
+int main()
+{
+    system("pause");
+    return 0;
+}
+
+
